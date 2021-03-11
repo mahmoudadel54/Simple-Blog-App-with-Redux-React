0552aadae339dd43f5e33b4c48026280b6e676dc
mahmoudadel54 <mahmoodmadel@gmail.com>
2021-03-11 15:16:47 +0200

    modify in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,18 @@ import BlogsListContent from "../components/blogsList";
 export default function Home() {
   return (
     <div className="content">
-      <h1>New Blogs</h1>
+        <h1
+        style={{
+          border: "solid",
+          borderRadius: "1em",
+          width: "fit-content",
+          margin: "auto",
+          padding: "0.2em 1em",
+        }}
+      >
+        Simple Blog App
+      </h1>
+
 
       <div className="container">
         <div className="home-grid">
